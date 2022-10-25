e0c3b6e3b64c4b75fc888effa562c9f71f65e0bc
kokejohh <kokejohh@gmail.com>
2022-10-26 00:26:21 +0700

    c++ question 1

---
+++ cpp_241_assig_2.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int isCorrect(int cats[], int cat, int n);
+void printArray(const char *message, int arr[], int n);
+
+int main()
+{
+	int n;
+	
+	cout << "input n:" ;
+	cin >> n;
+	
+	int cats[n], newCats[n], sortCats[n], status[n], flag = 0;
+	
+	for (int i = 0; i < n; i++)
+	{	
+		cin >> cats[i];
+		sortCats[i] = cats[i];
+		status[i] = newCats[i] = 0;
+	}
+	sort(sortCats, sortCats + n);
+	
+	printArray("cat", cats, n);
+	cout << endl;
+	
+	int result = isCorrect(cats, 1, n);
+	if (result)
+	{
+		cout << "box size : " << 0 << endl;
+		return 0;
+	}
+	for (int i = 0; i < n; i+=2)
+	{
+		int count = 0, cat = sortCats[i];
+		for (int j = 0; j < n; j++)
+			status[j] = newCats[j] = 0;
+		for (int j = 0; j < n - 1; j++)
+		{
+			if (cats[j] <= cat && status[j] == 0)
+			{
+				status[j] = 1;
+				newCats[count++] = cats[j];
+				for (int k = j + 1; k < n; k++)
+				{
+					if (cats[j] == cats[k])
+					{
+						status[k] = 1;
+						newCats[count++] = cats[k];	
+					}
+				}
+			}
+		}
+		for (int j = 0; j < n; j++)
+			if (status[j] == 0)
+				newCats[count++] = cats[j];
+		
+		cout << "size : " << cat << " ";
+		printArray("status", status, n);
+		printArray("newCats", newCats, n);
+		cout << endl;
+		
+		if(flag == 0 && (result = isCorrect(newCats, cat, n)))
+			flag = 1;
+	}
+	cout << "box size : " << result << endl;
+	return (0);
+}
+
+int isCorrect(int cats[], int cat, int n)
+{
+	for (int i = 0; i < n; i+=2)
+		if (cats[i] != cats[i + 1])
+			return 0;
+	return cat;
+}
+
+void printArray(const char *message, int arr[], int n)
+{
+	cout << message << " : ";
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+}
+
