7ada3725a1bd17c7cc038139e13595195c8b6436
kokejohh <kokejohh@gmail.com>
2022-11-27 01:05:03 +0700

    py2_1_2 change format

--- py_241_assign2_1_2.py
+++ py_241_assign2_1_2.py
@@ -3,32 +3,23 @@
 
 def correct(cats):
     for i in range(0, len(cats), 2):
-        if (cats[i] != cats[i + 1]):
-            return False
+        if (cats[i] != cats[i + 1]): return False
     return True
 
-def remainCat(cats):
-    tmp = cats[:]
-    for cat in tmp:
-        if (size == cat):
-            tmp.remove(cat)
-            tmp.remove(cat)
-            break
-    return tmp
-
+def remainCat(cats, size):
+    return [cat for cat in cats if size != cat]
 
 n = int(input('input n : '))
-
 cats = []
-for i in range(n):
-    cats.append(int(input()))
-    
+for i in range(n): cats.append(int(input()))
 setCat = set(cats)
-for size in setCat:
-    cats = remainCat(cats)
-    if (correct(cats)):
-        print(size)
-        break
+if (correct(cats)): print(0)
+else:
+    for size in setCat:
+        cats = remainCat(cats, size)
+        if (correct(cats)):
+            print(size)
+            break
 
 
 
