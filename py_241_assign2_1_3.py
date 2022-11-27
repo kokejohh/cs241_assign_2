a978b4a7c5f3350df6414b433b89259859d9ff21
kokejohh <kokejohh@gmail.com>
2022-11-27 12:07:47 +0700

    py2_1_3 algo by nuck

---
+++ py_241_assign2_1_3.py
@@ -0,0 +1,20 @@
+n = int(input('input n: '))
+
+def findIndex(arr, value):
+    return (i for i in range(len(arr)) if arr[i] == value)
+
+cats = []
+for i in range(n):
+    cats.append(int(input()))
+setCat = set(cats)
+
+ans = 0
+for size in setCat:
+    for i in range(len(cats)):
+        x, y = findIndex(cats, size)
+        tmp_max = max(cats[x + 1:y], default=0)
+        tmp_max = size if (tmp_max > size) else tmp_max
+        ans = tmp_max if (tmp_max > ans) else ans
+        break
+print(ans)
+    
